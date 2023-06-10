e2dff5f68a486126be486e8fde4e2581a16b3e0f
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-06-10 11:32:51 +0200

    Implement thwe API views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,139 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+# from rest_framework.authentication import TokenAuthentication
+from core.permissions import IsOwnerOrReadOnly
+from . serializers import (
+    StudentSerializer,
+    LandlordSerializer,
+    PropertySerializer,
+    PropertyImageSerializer,
+    AmenitySerializer,
+    CitySerializer,
+    ReviewSerializer,
+    BookingSerializer,
+    InstitutionSerializer
+)
+from . models import (
+    Student,
+    Landlord,
+    Property,
+    PropertyImage,
+    City,
+    Institution,
+    Amenity,
+    Review,
+    Booking,
+)
 
-# Create your views here.
+
+class StudentList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    
+
+class LandloardList(generics.ListCreateAPIView):
+    queryset = Landlord.objects.all()
+    serializer_class = LandlordSerializer
+
+
+class LandlordDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Landlord.objects.all()
+    serializer_class = LandlordSerializer
+    
+
+class PropertyList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PropertyDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+
+class PropertyImageList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = PropertyImage.objects.all()
+    serializer_class = PropertyImageSerializer
+
+class PropertyImageDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    queryset = PropertyImage.objects.all()
+    serializer_class = PropertyImageSerializer
+
+   
+class CityList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+
+class CityDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    
+
+class InstitutionList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Institution.objects.all()
+    serializer_class = InstitutionSerializer
+
+
+class InstitutionDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    queryset = Institution.objects.all()
+    serializer_class = InstitutionSerializer
+
+
+class ReviewList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class AmenityList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Amenity.objects.all()
+    serializer_class = AmenitySerializer
+    
+
+class AmenityDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    queryset = Amenity.objects.all()
+    serializer_class = AmenitySerializer
+
+
+class BookingList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
