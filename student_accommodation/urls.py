670bd9c4e5390b7f3da9b4a28181993c20b0bce4
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-06-10 10:53:03 +0200

    Change the root url patterns

--- student_accommodation/urls.py
+++ student_accommodation/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+from accounts.views import MyTokenObtainPairView
 
 urlpatterns = [
+    path('api/', include('core.urls')),
+    path('api/auth/', include('accounts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
