fb176f725a16fd526b580552ef696d349c9972a2
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-06-10 11:51:49 +0200

    Implement the accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from . custom_managers import CustomUserManager
 
-# Create your models here.
+class User(AbstractUser):
+    class Meta:
+        verbose_name_plural = "Users"
+        verbose_name = "User"
+
+    username = None
+    email = models.EmailField(max_length=255, unique=True, null=False, blank=False)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+    avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
+    is_student = models.BooleanField(default=False)
+    is_landlord = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+# from core.permissions import IsOwnerOrReadOnly
+from . serializers import UserSerializer, RegisterSerializer, MyTokenObtainPairSerializer
+from . models import User
 
-# Create your views here.
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+#Class based view to register user
+class RegisterView(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+# override the default token obtain view
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
