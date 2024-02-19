f3d118f453f14d95ecc5f27b87877d6bb96afecf
SirSanctified <mambambopritchard@gmail.com>
2024-02-19 12:40:03 +0200

    Update Dockerfile and page.tsx, fix compose.yaml formatting

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,8 @@
-import Link from "next/link";
-
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-        Student Accommodation System
+        Student Accommodation System (SAS)
       </h1>
     </main>
   );
