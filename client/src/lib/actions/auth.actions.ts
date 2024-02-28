a50c4a77c9a617f56928d25b34390dd3bcc5c957
SirSanctified <mambambopritchard@gmail.com>
2024-02-28 15:00:18 +0200

    Refactor auth.actions.ts

--- client/src/lib/actions/auth.actions.ts
+++ client/src/lib/actions/auth.actions.ts
@@ -1,26 +1,7 @@
 "use server";
 import type { User } from "@/store/store";
-import type { ErrorResponse, LoginType, RegisterType } from "@/types";
+import type { ErrorResponse, RegisterType } from "@/types";
 import axios, { type AxiosError } from "axios";
-
-export const login = async (user: LoginType): Promise<User> => {
-  try {
-    const response = await axios.post(
-      `http://server:8000/api/auth/login/`,
-      user,
-      {
-        withCredentials: true,
-      },
-    );
-    return response.data as User;
-  } catch (error) {
-    throw new Error(
-      (error as AxiosError)?.response?.data
-        ? ((error as AxiosError).response?.data as ErrorResponse)?.detail
-        : (error as AxiosError).message,
-    );
-  }
-};
 
 export const register = async (user: RegisterType): Promise<User> => {
   try {
@@ -32,18 +13,6 @@ export const register = async (user: RegisterType): Promise<User> => {
       },
     );
     return response.data as User;
-  } catch (error) {
-    throw new Error(
-      (error as AxiosError)?.response?.data
-        ? ((error as AxiosError).response?.data as ErrorResponse)?.detail
-        : (error as AxiosError).message,
-    );
-  }
-};
-
-export const logout = async (): Promise<void> => {
-  try {
-    await axios.post("/api/auth/logout", { withCredentials: true });
   } catch (error) {
     throw new Error(
       (error as AxiosError)?.response?.data
