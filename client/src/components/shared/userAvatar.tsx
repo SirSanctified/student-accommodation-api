626420569cd8d57a4ebeb12a019a3bb0ea2e22d4
SirSanctified <mambambopritchard@gmail.com>
2024-03-01 12:58:51 +0200

    Update import statement for User type

--- client/src/components/shared/userAvatar.tsx
+++ client/src/components/shared/userAvatar.tsx
@@ -1,5 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import type { User } from "@/store/store";
+import type { User } from "@/types";
 
 export function UserAvatar({ user }: { user: User }) {
   return (
