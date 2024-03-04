abdcf3bafbfdad574ef863efe7bc8f03190b3b24
SirSanctified <mambambopritchard@gmail.com>
2024-03-04 15:43:44 +0200

    Remove unused imports and update property form

--- client/src/components/shared/properties/propertyForm.tsx
+++ client/src/components/shared/properties/propertyForm.tsx
@@ -1,7 +1,12 @@
 import type { PropertyFormProps } from "@/types";
 
 const PropertyForm = ({ action, propertyData }: PropertyFormProps) => {
-  return <form>PropertyForm</form>;
+  return (
+    <form>
+      <p>{propertyData ? "Edit" : "Add"}</p>
+      <input type="submit" value={action} />
+    </form>
+  );
 };
 
 export default PropertyForm;
