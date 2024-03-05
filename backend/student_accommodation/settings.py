59b5ed0f2afe4d0251b76717c656cc1b3a904b9f
SirSanctified <mambambopritchard@gmail.com>
2024-03-05 10:32:09 +0200

    Add default permission classes to REST framework settings and update URL configuration

--- backend/student_accommodation/settings.py
+++ backend/student_accommodation/settings.py
@@ -138,6 +138,9 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "accounts.authentication.CustomAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
--- backend/student_accommodation/urls.py
+++ backend/student_accommodation/urls.py
@@ -19,11 +19,10 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
-from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
+schema_view = get_schema_view(  # pylint: disable=invalid-name
     openapi.Info(
         title="REST APIs",
         default_version="v1",
@@ -33,17 +32,17 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=(),
 )
 
 urlpatterns = [
     path(
-        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
-    ),
-    path(
-        "/",
+        "",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
+    ),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("api/", include("core.urls")),
