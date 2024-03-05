23eaa941e4ef2cb94f08287f84ba6c41ec807825
SirSanctified <mambambopritchard@gmail.com>
2024-03-05 10:55:58 +0200

    Delete unused migrations and update PropertyViewSet in core/views.py

--- backend/core/views.py
+++ backend/core/views.py
@@ -60,6 +60,14 @@ class PropertyViewSet(ModelViewSet):
     """
 
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filterset_fields = ["city__name", "owner__last_name", "amenities__name"]
+    search_fields = [
+        "name",
+        "city__name",
+        "location",
+        "street",
+        "owner__last_name",
+    ]
     queryset = Property.objects.all()  # pylint: disable=no-member
     serializer_class = PropertySerializer
 
@@ -206,7 +214,7 @@ class RoomViewSet(ModelViewSet):
         "property__street",
         "property__owner__last_name",
     ]
-    filter_fields = [
+    filterset_fields = [
         "name",
         "property__city__name",
         "num_beds",
