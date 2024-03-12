24605961e7b003dbfd9d3785222fc3f4dacbfd0f
SirSanctified <mambambopritchard@gmail.com>
2024-03-12 13:23:40 +0200

    Add RoomsList component

---
+++ client/src/components/shared/properties/roomsList.tsx
@@ -0,0 +1,17 @@
+import type { RoomsListProps } from "@/types";
+import RoomCard from "./roomCard";
+
+const RoomsList = ({ rooms }: { rooms: RoomsListProps[] }) => {
+  return (
+    <div className="flex w-full flex-col flex-wrap items-center justify-center gap-4  md:flex-row">
+      {rooms.map((room, index) => (
+        <>
+          <RoomCard key={index} {...room} />
+          <RoomCard key={index} {...room} />
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default RoomsList;
