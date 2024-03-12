a28bebb4177a59cf58fd8b4493b1db52fda7bdf6
SirSanctified <mambambopritchard@gmail.com>
2024-03-12 13:29:38 +0200

    Refactor property details page layout and content

--- client/src/app/(dashboard)/profile/[id]/landlords/properties/[propertyId]/page.tsx
+++ client/src/app/(dashboard)/profile/[id]/landlords/properties/[propertyId]/page.tsx
@@ -16,15 +16,19 @@ const PropertyDetails = async ({
     },
   );
   return (
-    <div className="w-full">
+    <main className="w-full">
       <h1 className="mb-2 mt-4 text-3xl font-bold text-indigo-950">
-        Property Details
+        {property.data.name} - {property.data.city}, {property.data.location}
       </h1>
       <p className="mb-4 max-w-xl text-indigo-950">
-        This page contains all the details of your property {property.data.name}
-        .
+        {property.data.property_type[0]?.toUpperCase() +
+          property.data.property_type.slice(1)}{" "}
+        in {property.data.street}, {property.data.number} (
+        {property.data.rooms?.length} rooms)
       </p>
-      <p className="mb-4 max-w-xl text-indigo-950"></p>
+      <h2 className="my-4 w-full text-start text-2xl font-bold text-indigo-950">
+        Rooms
+      </h2>
       {property.data?.rooms?.length ? (
         <RoomsList
           rooms={property.data.rooms.map((room) => ({
@@ -38,7 +42,7 @@ const PropertyDetails = async ({
       >
         <Button variant="outline">Update Property</Button>
       </Link>
-    </div>
+    </main>
   );
 };
 
